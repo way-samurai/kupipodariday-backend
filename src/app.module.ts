769ddcefb097e269b8affd8c14116e6b5b97d1e8
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-06-16 17:14:47 +0300

    Подключил winston

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
 import { configSchema } from './config/joi.shema';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 @Module({
   imports: [
@@ -9,6 +11,19 @@ import { configSchema } from './config/joi.shema';
       isGlobal: true,
       load: [configuration],
       validationSchema: configSchema,
+    }),
+    WinstonModule.forRoot({
+      levels: {
+        critical_error: 0,
+        error: 1,
+        special_warning: 2,
+        another_log_level: 3,
+        info: 4,
+      },
+      transports: [
+        new winston.transports.Console({ format: winston.format.simple() }),
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      ],
     }),
   ],
   controllers: [],
