041e515bb822d032532a8d61230686005fc20e58
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-06-16 16:51:22 +0300

    Подключил валидацию конфигурации сервера

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
+import { configSchema } from './config/joi.shema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
+      validationSchema: configSchema,
     }),
   ],
   controllers: [],
