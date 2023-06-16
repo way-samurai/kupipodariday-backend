6291e8ba6f6cf41378ada9515a6664fa7342e4be
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-06-16 18:33:29 +0300

    зарегестрировал helmet в Express

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
