2e03afbb3663e22ba1bab0b2f9ab8be35663aa44
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-06-18 02:59:39 +0300

    Описал сервисы и контроллер для wishes. Подключил сервисы к репозиторию

--- src/wishes/wishes.controller.ts
+++ src/wishes/wishes.controller.ts
@@ -1,42 +1,74 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
-import { WishesService } from './wishes.service';
-import { CreateWishDto } from './dto/create-wish.dto';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { UpdateWishDto } from './dto/update-wish.dto';
+import { WishesService } from './wishes.service';
+import { User } from 'src/users/entities/user.entity';
+import { Wish } from './entities/wish.entity';
 
 @Controller('wishes')
 export class WishesController {
   constructor(private readonly wishesService: WishesService) {}
 
-  @Post()
-  create(@Body() createWishDto: CreateWishDto) {
-    return this.wishesService.create(createWishDto);
+  @Get('last')
+  getLastWishes(): Promise<Wish[]> {
+    return this.wishesService.getLast();
   }
 
-  @Get()
-  findAll() {
-    return this.wishesService.findAll();
+  @Get('top')
+  getTopWishes(): Promise<Wish[]> {
+    return this.wishesService.getTop();
   }
 
+  @UseGuards(JwtGuard)
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.wishesService.findOne(+id);
+  getById(@Param('id') id: string): Promise<Wish> {
+    return this.wishesService.getById(+id);
   }
 
+  @UseGuards(JwtGuard)
+  @Post()
+  create(
+    @Body() CreateWishDto,
+    @Req() req: Request & { user: User },
+  ): Promise<Wish> {
+    return this.wishesService.create(CreateWishDto, req.user.id);
+  }
+
+  @UseGuards(JwtGuard)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWishDto: UpdateWishDto) {
-    return this.wishesService.update(+id, updateWishDto);
+  update(
+    @Param('id') id: string,
+    @Req() req: Request & { user: User },
+    @Body() updateWishDto: UpdateWishDto,
+  ): Promise<unknown> {
+    return this.wishesService.updateOne(+id, req.user.id, updateWishDto);
   }
 
+  @UseGuards(JwtGuard)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.wishesService.remove(+id);
+  remove(
+    @Param('id') id: string,
+    @Req() req: Request & { user: User },
+  ): Promise<unknown> {
+    return this.wishesService.removeOne(+id, req.user.id);
+  }
+
+  @UseGuards(JwtGuard)
+  @Post(':id/copy')
+  copy(
+    @Param('id') wishId: string,
+    @Req() req: Request & { user: User },
+  ): Promise<unknown> {
+    return this.wishesService.copiedWish(+wishId, req.user.id);
   }
 }
--- src/wishes/wishes.service.ts
+++ src/wishes/wishes.service.ts
@@ -1,26 +1,116 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  DataSource,
+  FindManyOptions,
+  FindOneOptions,
+  Repository,
+} from 'typeorm';
 import { CreateWishDto } from './dto/create-wish.dto';
 import { UpdateWishDto } from './dto/update-wish.dto';
+import { Wish } from './entities/wish.entity';
+import {
+  ALREADY_OWNED,
+  RAISED_NOT_NULL,
+  USER_NOT_OWNER,
+} from 'src/utils/constants/wishes';
 
 @Injectable()
 export class WishesService {
-  create(createWishDto: CreateWishDto) {
-    return 'This action adds a new wish';
+  constructor(
+    private dataSource: DataSource,
+    @InjectRepository(Wish)
+    private wishesRepository: Repository<Wish>,
+  ) {}
+
+  findAll(query: FindManyOptions<Wish>): Promise<Wish[]> {
+    return this.wishesRepository.find(query);
   }
 
-  findAll() {
-    return `This action returns all wishes`;
+  findOne(query: FindOneOptions<Wish>): Promise<Wish> {
+    return this.wishesRepository.findOne(query);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} wish`;
+  getLast(): Promise<Wish[]> {
+    return this.findAll({ order: { createdAt: 'DESC' }, take: 40 });
   }
 
-  update(id: number, updateWishDto: UpdateWishDto) {
-    return `This action updates a #${id} wish`;
+  getTop(): Promise<Wish[]> {
+    return this.findAll({ order: { copied: 'DESC' }, take: 10 });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} wish`;
+  getById(id: number): Promise<Wish> {
+    return this.findOne({ where: { id }, relations: { owner: true } });
+  }
+
+  create(createWishDto: CreateWishDto, ownerId: number): Promise<Wish> {
+    const wish = this.wishesRepository.create({
+      ...createWishDto,
+      owner: { id: ownerId },
+    });
+    return this.wishesRepository.save(wish);
+  }
+
+  async updateOne(
+    id: number,
+    userId: number,
+    updateWishDto: UpdateWishDto,
+  ): Promise<unknown> {
+    const wish = await this.findOne({
+      where: { id },
+      relations: { owner: true },
+    });
+    if (userId !== wish.owner.id) {
+      throw new ForbiddenException(USER_NOT_OWNER);
+    }
+    if (updateWishDto.price && wish.raised > 0) {
+      throw new ForbiddenException(RAISED_NOT_NULL);
+    }
+    return this.wishesRepository.update(id, updateWishDto);
+  }
+
+  async removeOne(id: number, userId: number): Promise<unknown> {
+    const wish = await this.findOne({
+      where: { id },
+      relations: { owner: true },
+    });
+    if (userId !== wish.owner.id) {
+      throw new ForbiddenException(USER_NOT_OWNER);
+    }
+
+    this.wishesRepository.delete(id);
+    return wish;
+  }
+
+  async copiedWish(wishId: number, userId: number): Promise<unknown> {
+    const wish = await this.findOne({
+      where: { id: wishId },
+    });
+    const { name, description, image, link, price, copied } = wish;
+
+    const findWish = !!(await this.findOne({
+      where: { name, link, price, owner: { id: userId } },
+      relations: { owner: true },
+    }));
+
+    if (findWish) {
+      throw new ForbiddenException(ALREADY_OWNED);
+    }
+
+    const wishCopy = {
+      name,
+      description,
+      image,
+      link,
+      price,
+      owner: { id: userId },
+    };
+
+    await this.dataSource.transaction(async (transManager) => {
+      await transManager.update<Wish>(Wish, wishId, { copied: copied + 1 });
+      await transManager.insert<Wish>(Wish, wishCopy);
+    });
+
+    return {};
   }
 }
