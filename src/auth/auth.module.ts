7a7e99f74451318aaca31e5af930555df5f0715f
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-06-17 21:06:23 +0300

    --amend

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [UsersModule, PassportModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   exports: [AuthModule],
 })
 export class AuthModule {}
