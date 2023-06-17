f1a76c59690dd224266053a429496b43ac44e6a2
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-06-18 01:28:31 +0300

    Описал методы контроллеров и сервисы для users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,33 +5,54 @@ import {
   Body,
   Patch,
   Param,
-  Delete,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { User } from './entities/user.entity';
+import { SearchUserDto } from './dto/search-user.dto';
+import { Wish } from 'src/wishes/entities/wish.entity';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  // @Post()
-  // create(@Body() createUserDto: CreateUserDto) {
-  //   return this.usersService.create(createUserDto);
-  // }
+  @Get('me')
+  async getUser(@Req() req: Request & { user: User }): Promise<User> {
+    return req.user;
+  }
 
-  // @Get()
-  // findAll() {
-  //   return this.usersService.findAll();
-  // }
+  @Get('me/wishes')
+  async getMyWishes(
+    @Req() req: Request & { user: User },
+  ): Promise<Wish | Wish[]> {
+    return this.usersService.getMyWishes(req.user.id);
+  }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.usersService.findOne(+id);
-  // }
+  @Get(':username')
+  async getByUsername(@Param('username') username: string): Promise<User> {
+    return this.usersService.getByUsername(username);
+  }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-  //   return this.usersService.update(+id, updateUserDto);
-  // }
+  @Get(':username/wishes')
+  getUserWishes(@Param('username') username: string): Promise<Wish | Wish[]> {
+    return this.usersService.getUserWishes(username);
+  }
+
+  @Patch('me')
+  updateUser(
+    @Body() updateUserDto: UpdateUserDto,
+    @Req() req: Request & { user: User },
+  ): Promise<User> {
+    return this.usersService.updateOne(req.user.id, updateUserDto);
+  }
+
+  @Post('find')
+  findByUserNameOrEmail(@Body() searchUserDto: SearchUserDto): Promise<User[]> {
+    const { query } = searchUserDto;
+    return this.usersService.findByQuery(query);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,12 +2,15 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { HashService } from 'src/hash/hash.service';
 import { User } from './entities/user.entity';
-import { FindOneOptions, Repository } from 'typeorm';
+import { FindManyOptions, FindOneOptions, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import {
   USER_ALREADY_EMAIL_EXIST,
   USER_ALREADY_USERNAME_EXIST,
 } from 'src/utils/constants/users';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { plainToClass } from 'class-transformer';
+import { Wish } from 'src/wishes/entities/wish.entity';
 
 @Injectable()
 export class UsersService {
@@ -41,9 +44,69 @@ export class UsersService {
     return this.usersRepository.save(newUser);
   }
 
+  async findAll(query: FindManyOptions<User>): Promise<User[]> {
+    return this.usersRepository.find(query);
+  }
+
   async findOne(id: FindOneOptions<User>): Promise<User> {
     const user = await this.usersRepository.findOne(id);
-
     return user;
+  }
+
+  findByQuery(query: string): Promise<User[]> {
+    return this.findAll({
+      where: [{ username: query }, { email: query }],
+    });
+  }
+
+  async updateOne(id: number, updateUserDto: UpdateUserDto) {
+    const { email, username, password } = updateUserDto;
+
+    const user = await this.findOne({ where: { id } });
+
+    if (email) {
+      const emailInBase = await this.findOne({ where: { email } });
+
+      if (emailInBase && emailInBase.id !== id) {
+        throw new ConflictException(USER_ALREADY_EMAIL_EXIST);
+      }
+    }
+
+    if (username) {
+      const userNameInBase = await this.findOne({ where: { username } });
+
+      if (userNameInBase && userNameInBase.id !== id) {
+        throw new ConflictException(USER_ALREADY_USERNAME_EXIST);
+      }
+    }
+
+    if (password) {
+      updateUserDto.password = await this.hashService.generate(password);
+    }
+
+    const updateUser = { ...user, ...updateUserDto };
+    await this.usersRepository.update(id, updateUser);
+
+    return this.findOne({ where: { id } });
+  }
+
+  getByUsername(username: string): Promise<User> {
+    return this.findOne({ where: { username } }).then((user) =>
+      plainToClass(User, user, { excludePrefixes: ['password'] }),
+    );
+  }
+
+  getMyWishes(userId: number): Promise<Wish | Wish[]> {
+    return this.findOne({
+      where: { id: userId },
+      relations: { wishes: { owner: true } },
+    }).then((user) => user.wishes);
+  }
+
+  getUserWishes(username: string): Promise<Wish | Wish[]> {
+    return this.findOne({
+      where: { username },
+      relations: { wishes: true },
+    }).then((user) => user.wishes);
   }
 }
