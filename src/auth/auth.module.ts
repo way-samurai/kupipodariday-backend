4302f29d8141d85f6b8850ded016eab5a5a8fd96
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-06-17 21:05:37 +0300

    Подключил PassportModule к auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [UsersModule, PassportModule],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthModule],
 })
 export class AuthModule {}
